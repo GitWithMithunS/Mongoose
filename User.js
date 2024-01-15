7884d8bd45da1fadc9719a751cf85ede803f96d6
Mithun s <chakravarthimithun62@gmail.com>
2024-01-16 00:53:14 +0530

    'validation'

--- User.js
+++ User.js
@@ -1,8 +1,52 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-    name:String,
-    age:Number
+// const userSchema = new mongoose.Schema({      //a simple schema
+//     name:String,
+//     age:Number,
+//     email:String,
+//     createdAt: Date,
+//     updatedAt: Date,
+//     hobbies : [String],
+//     address:{
+//         Street:String,
+//         city:String
+//     }
+// })
+
+const addressSchema = new mongoose.Schema({
+    Street: String,
+    city: String
 })
 
-module.exports = mongoose.model('User',userSchema) //User here is the name of the collection inside database of mongodb with which ur interacting.
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: {
+        type: Number, 
+        min: 1,              //minimum and maximum number that can be entered 
+        max: 100,
+        validate : {                // validate is used to write custom validation by the programmer
+            validator : v => v % 1 ,       //only users created by save(), create() method go through validation process. any other updation methods do not go through validation as the directly work with the database.
+            message : props => `${props.value} is not an even number`
+        }
+    },
+    email: {
+        type: String,
+        minLength:3,          // email should be of atleast 3charecters
+        required: true,
+        lowercase: true,        //irrespective of wt user enter the email will be stored in lowercase 
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,       //can not be changed once set
+        default: () => Date.now()    // default : Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    // bestFriend: {type:mongoose.SchemaType.ObjectId,ref:'User'},
+    hobbies: [String],
+    address: addressSchema
+})
+
+module.exports = mongoose.model('User', userSchema) //User here is the name of the collection inside database of mongodb with which ur interacting.
--- script.js
+++ script.js
@@ -2,25 +2,52 @@ const mongoose = require('mongoose')
 const User = require('./User')
 
 mongoose.connect('mongodb://localhost:27017/mongoose?directConnection=true')
-// .then( 
-//     console.log('connected to mongodb')
-// )
-// .catch((err) => {
-//     console.log('some error occured while connecting to mongodb',err)
-// })        //this lines may not be required while using mongoose as it internally takes care of all this.
+//     .then(
+//         console.log('connected to mongodb')
+//     )
+//     .catch((err) => {
+//         console.log('some error occured while connecting to mongodb', err)
+//     })        //this lines may not be required while using mongoose as it internally takes care of all this.
 
-//creating a document inside the collection
+// creating a document inside the collection
 
-//their are 2 methods 
-// async function run() {                     //1.
-//     const user =new User({name:'mithun' , age:21})
+// their are 2 methods
+// async function run() {                     //1.save() method ....by creating a new instance of a 'User' model using mongoose.(in mongoose a model is constructor function) and then saving the user to the database
+//     const user = new User({ name: 'mithun', age: 21 })
 //     await user.save()
-//     .then(()=>{console.log('user data is saved',user)})
+//         .then(() => { console.log('user data is saved', user) })
 // }
 
-async function run() {                      //2. create() method
-    const user =await User.create({name:'mithun' , age:22})
-    await user.save()
-    .then(()=>{console.log('user data is saved',user)})
+// async function run() {                      //2. create() method to create a new user
+//     const user = await User.create({          //note:- User.findOne,findMany,updateOne,Many all this methods skip validation process(they do not go through validation).so do not use it.instead u can do  this way User.findById().save() get ur user and call save() on it.
+//         name: 'mithun',
+//         age: 22,
+//         hobbies: ['swimming', "dancing"],
+//         address: { Street: 'attibele', city: 'bangalore' }
+//     })
+//     // user.name='ashish'       //the variables can be changed in the later part also before it is saved in the db
+//     await user.save()           //saving the user in the db
+//         .then(() => { console.log('user data is saved', user) })
+// }
+
+async function run() {                      //2. create() method to create a new user
+    try {
+        const user = await User.create({     //create() method automatically saves the user dtat in the mongodb
+            name: 'mithun',
+            email: 'mia@gmail.com',
+            age: 20,
+            hobbies: ['swimming', "dancing"],
+            address: { Street: 'attibele', city: 'bangalore' }
+        })
+        // user.name='ashish'       //the variables can be changed in the later part also before it is saved in the db
+        // await user.save()           //saving the user in the db
+        // user.createdAt = 5           //as createdAt is immutable it can not be changed even after accesing it. 
+        // await user.save()          
+        console.log(user)
+    } catch (e) {
+        console.log('come eror occured', e.message)
+        // console.log(e.errors)     //to get more details of the error
+    }
 }
+
 run()   
